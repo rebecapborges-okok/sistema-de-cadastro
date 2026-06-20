29510407157144b749cd0b986c05a5053b9c8fcd
Rebeca P. Borges <rebecapborges@gmail.com>
2026-06-20 15:10:11 -0400

    Refactor client registration into a function

--- ##cadasttrar clientes imoveis 2.py
+++ ##cadasttrar clientes imoveis 2.py
@@ -1,45 +1,68 @@
 ##cadasttrar clientes imoveis 2
-
+def cadastrar_cliente():
 ##cliente
-Cadastro= {
-    'Cliente': input("Digite o nome do cliente"),
-    'Telefone': input("Digite o telefone do cliente"),
-    'Email': input("Digite o email do cliente"),
-    'Endereço': input("Digite o endereço do cliente")
-}
-    ##tipo de atendimento
-##compra ou venda???'
-
-tipo_atendimento = input("Compra ou venda?").lower()
-    
-if tipo_atendimento in ["compra", "comprar"]:
-    print("Cliente deseja comprar")
-    ##imovel para comprar
-    compra = {
-        "Tipo": input("Casa, Terreno ou fazenda?"),
-        'Valor': input("Valor que o cliente quer comprar"),
-        'Tamanho' : input("Que tamanho precisa ter? Insira o valor em m²"),
-        'Peças': input("Quantas peças precisam ter a casa?"),
-        'Localizacao': input("Qual a localização desejada do imóvel?")
+    Cadastro= {
+        'Cliente': input("Digite o nome do cliente"),
+        'Telefone': input("Digite o telefone do cliente"),
+        'Email': input("Digite o email do cliente"),
+        'Endereço': input("Digite o endereço do cliente")
     }
-    Cadastro["Imovel"]= compra
-    print(Cadastro)
-elif tipo_atendimento in ["vender","venda"]:
-    print("Cliente deseja vender")
-    venda= {
-    "Tipo": input("Casa, Terreno ou fazenda?"),
-    'Valor': input("Valor que o cliente quer vender"),
-    'Negociavel': input("O cliente aceita fazer negócio?"),
-    'Tamanho': input("Insira o valor em m²"),
-    'Peças': input("Quantas peças há na casa?"),
-    'Localizacao': input("Qual a localização do imóvel?")
-}
-    Cadastro["Imovel"]= venda
-    print(Cadastro)
-else:
-    print("Digite uma resposta válida") 
+    
+    ##tipo de atendimento
+##compra ou venda???
+
+    tipo_atendimento = input("Compra ou venda?").lower()
+    
+    if tipo_atendimento in ["compra", "comprar"]:
+        print("Cliente deseja comprar")
+    ##imovel para comprar
+        compra = {
+            "Tipo": input("Casa, Terreno ou fazenda?"),
+            'Valor': input("Valor que o cliente quer comprar"),
+            'Tamanho' : input("Que tamanho precisa ter? Insira o valor em m²"),
+            'Peças': input("Quantas peças precisam ter a casa?"),
+            'Localizacao': input("Qual a localização desejada do imóvel?")
+        }
+        Cadastro["Imovel"]= compra
+        print(Cadastro)
+
+    elif tipo_atendimento in ["vender","venda"]:
+        print("Cliente deseja vender")
+        venda= {
+            "Tipo": input("Casa, Terreno ou fazenda?"),
+            'Valor': input("Valor que o cliente quer vender"),
+            'Negociavel': input("O cliente aceita fazer negócio?"),
+            'Tamanho': input("Insira o valor em m²"),
+            'Peças': input("Quantas peças há na casa?"),
+            'Localizacao': input("Qual a localização do imóvel?")
+    }
+        Cadastro["Imovel"]= venda
+        print(Cadastro)
+
+    else:
+        print("Digite uma resposta válida") 
+    return Cadastro
 
 cadastros = []
-cadastros.append(Cadastro)
+cadastros.append(cadastrar_cliente())
 
 print(cadastros)
+
+novo_cadastro = input("Deseja cadastrar outro cliente?").lower()
+
+while novo_cadastro in ["sim", "s"]:
+    cadastros.append(cadastrar_cliente())
+    novo_cadastro = input("Deseja cadastrar outro cliente?").lower()
+for cliente in cadastros:
+        print(cliente)
+        
+if novo_cadastro in["nao", "n", "não", "ñ"]:
+    print("Cadastro concluído")
+    
+nome_busca = input("Digite o nome do cliente: ").lower()
+for cliente in cadastros:
+    if cliente["Cliente"].lower() == nome_busca:
+        print(cliente)
+        encontrado =True
+    if encontrado == False:
+        print("Cliente não encontrado")
